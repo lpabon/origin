9c70022c0ab0532c98bcb65f5a530d1398b47c5e
Humble Devassy Chirammal <humble.devassy@gmail.com>
2016-06-02 18:30:41 +0530

    Update provision-util.sh

--- contrib/vagrant/provision-util.sh
+++ contrib/vagrant/provision-util.sh
@@ -236,7 +236,7 @@ os::provision::base-provision() {
       iptables -A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 24007:24047 -j ACCEPT
       iptables -A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 111 -j ACCEPT
       iptables -A RH-Firewall-1-INPUT -m state --state NEW -m udp -p udp --dport 111 -j ACCEPT 
-      iptables -A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 38465:38467 -j ACCEPT
+      iptables -A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 49152:49251 -j ACCEPT
 
       # Allow all!
       iptables -I INPUT -j ACCEPT
